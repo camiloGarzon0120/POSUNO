643ce1881aca12af297a062e9b513c6944e0708d
camiloGarzon0120 <camilogarzon0120@hotmail.com>
2021-07-11 16:17:54 -0500

    class video 10, interface Products, GetListAsync customers and products

--- POSUNO/POSUNO/POSUNO.Shared/Pages/CustomersPage.xaml.cs
+++ POSUNO/POSUNO/POSUNO.Shared/Pages/CustomersPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using POSUNO.Models;
+﻿using POSUNO.Components;
+using POSUNO.Helpers;
+using POSUNO.Models;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -7,6 +9,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,9 +25,40 @@ namespace POSUNO.Pages
     {
         public CustomersPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
         }
         public ObservableCollection<Customer> Customers { get; set; }
 
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            loadCustomerAsync();
+        }
+
+        private async void loadCustomerAsync()
+        {
+            Loader loader = new Loader("Por favor espere");
+            loader.Show();
+            Response response = await ApiService.GetListAsync<Customer>("Customers");
+            loader.Close();
+
+            if(!response.IsSuccess)
+            {
+                MessageDialog dialog = new MessageDialog(response.Message, "Error");
+                await dialog.ShowAsync();
+                return;
+            }
+
+            List<Customer> customers = (List<Customer>)response.Result;
+            Customers = new ObservableCollection<Customer>(customers);
+            RefreshList();
+        }
+
+        private void RefreshList()
+        {
+            CustomersListView.ItemsSource = null;
+            CustomersListView.Items.Clear();
+            CustomersListView.ItemsSource = Customers;
+        }
     }
 }
