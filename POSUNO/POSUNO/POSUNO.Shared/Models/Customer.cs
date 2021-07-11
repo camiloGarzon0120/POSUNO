fc34fc96c6b0f658c676868976e809cfe2617c23
camiloGarzon0120 <camilogarzon0120@hotmail.com>
2021-07-11 15:21:33 -0500

    class video 9, interface Customers

---
+++ POSUNO/POSUNO/POSUNO.Shared/Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POSUNO.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FirtName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Adrress { get; set; }
+        public string Email { get; set; }
+        public bool IsActive { get; set; }
+        public User User { get; set; }
+    }
+}
